269db61e371eef6d0b7d98547398bcf702cb141c
download13 <download333@gmail.com>
2012-02-21 20:31:33 -0800

    Forgot to add new files

---
+++ src/com/mojang/mojam/network/packet/MouseMoveCommand.java
@@ -0,0 +1,36 @@
+package com.mojang.mojam.network.packet;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import com.mojang.mojam.network.NetworkCommand;
+
+
+public class MouseMoveCommand extends NetworkCommand {
+	private int mouseX;
+	private int mouseY;
+	
+	public MouseMoveCommand(int x, int y) {
+		this.mouseX = x;
+		this.mouseY = y;
+	}
+	
+	public void read(DataInputStream dis) throws IOException {
+		mouseX = dis.readInt();
+		mouseY = dis.readInt();
+	}
+	
+	public void write(DataOutputStream dos) throws IOException {
+		dos.writeInt(mouseX);
+		dos.writeInt(mouseY);
+	}
+	
+	public int getX() {
+		return mouseX;
+	}
+	
+	public int getY() {
+		return mouseY;
+	}
+}
